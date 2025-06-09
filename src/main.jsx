c96b06fc08e8d743e83013bc2ae47b88dbb253e3
rhk08 <140364137+rhk08@users.noreply.github.com>
2025-06-09 22:09:33 +0930

    fix 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from 'react-router';
+import { BrowserRouter } from 'react-router-dom';
 import './gsap-brand.css';
 import './index.css';
 import './styles/personlized-stylesheet.css';
@@ -8,7 +8,7 @@ import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/world-museum-website-deployed/">
       <App />
     </BrowserRouter>
   </StrictMode>
